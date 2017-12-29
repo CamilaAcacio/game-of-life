a735c699b40d3226ef38675a4c9cc5678a903015
camila paiva <camila.acacio.paiva@gmail.com>
2017-12-29 11:18:58 -0200

    Test5

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridWriter.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridWriter.java
@@ -14,17 +14,17 @@ public class GridWriter {
 	// Functions returns the status of the cells in the grid as a string, for printing
     public String convertToString(final Cell[][] gridContents) {
         StringBuffer printedGrid = new StringBuffer();
-	    for (Cell[] row : gridContents) {
-            for (Cell cell : row) {
-                printedGrid.append(cell.toString()); // Save the cell's status for printing
-            }
-            
-			// Add seperator to denote the next row
-			// Note: does not add seperator for empty rows
-            if (row.length > 0) {
-                printedGrid.append(LINE_SEPARATOR);
-            }
-        }
+//	    for (Cell[] row : gridContents) {
+//            for (Cell cell : row) {
+//                printedGrid.append(cell.toString()); // Save the cell's status for printing
+//            }
+//            
+//			// Add seperator to denote the next row
+//			// Note: does not add seperator for empty rows
+//            if (row.length > 0) {
+//                printedGrid.append(LINE_SEPARATOR);
+//            }
+//        }
         return printedGrid.toString();
     }
 }
